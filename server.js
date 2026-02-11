c9e8f26ddf2a93bc106b4dbe9788db9442e4fb75
PallavSharma9 <pallavsharma0202@gmail.com>
2026-02-11 12:54:02 +0530

    adding express backend on port 3000

--- server.js
+++ server.js
@@ -1,2 +1,15 @@
 // initializing package.json with npm init -y
 // installing express package by command npm install express
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/about", (req, res) => {
+  res.send("About Page");
+});
+
+app.listen(3000);
