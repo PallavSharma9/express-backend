d02778f9c4dba09f917dc09fca0c7b7814dc85c7
PallavSharma9 <pallavsharma0202@gmail.com>
2026-02-11 21:48:52 +0530

    adding patch

--- src/app.js
+++ src/app.js
@@ -27,4 +27,20 @@ app.delete("/notes/:id", (req, res) => {
     message: "selected task has been deleted",
   });
 });
+
+app.patch("/notes/:id", (req, res) => {
+  const description = req.body.description;
+
+  const index = Number(req.params.id);
+
+  if (index < 0 || index > notes.length) {
+    return res.status(404).json({
+      message: "data does not exist",
+    });
+  }
+
+  notes[index].description = description;
+
+  res.status(200).json("description has been updated");
+});
 export default app;
