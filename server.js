7f69ee92d91dc9f12b837fddded2a8db40fc5183
PallavSharma9 <pallavsharma0202@gmail.com>
2026-02-12 21:09:48 +0530

    connecting mongodb

--- server.js
+++ server.js
@@ -1,4 +1,13 @@
+import dns from "node:dns/promises";
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+// Force reliable DNS resolvers due to SRV resolution issues on local network
+import dotenv from "dotenv";
+dotenv.config();
+import { connectDB } from "./src/db/db.js";
+
 import app from "./src/app.js";
+
+await connectDB();
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
